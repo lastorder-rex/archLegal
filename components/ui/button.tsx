b38b9df56f43f4d6bd0005c3683c14b1d2e998f8
lastorder-rex <herewego.five@gmail.com>
2025-10-08 23:57:33 +0900

    chore: support ghost variant for Button

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -3,7 +3,7 @@ import clsx from 'clsx';
 import { Slot } from '@radix-ui/react-slot';
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
-  variant?: 'primary' | 'outline' | 'sidebar-primary';
+  variant?: 'primary' | 'outline' | 'sidebar-primary' | 'ghost';
   size?: 'sm' | 'default' | 'lg';
   asChild?: boolean;
 };
@@ -18,6 +18,8 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
         ? 'bg-primary text-primary-foreground hover:opacity-85 focus-visible:ring-primary'
         : variant === 'sidebar-primary'
         ? 'bg-[hsl(var(--sidebar-primary))] text-[hsl(var(--sidebar-primary-foreground))] hover:opacity-85 focus-visible:ring-[hsl(var(--sidebar-primary))]'
+        : variant === 'ghost'
+        ? 'bg-transparent text-primary hover:bg-primary/10 focus-visible:ring-primary focus-visible:ring-opacity-40'
         : 'border border-primary bg-background text-primary hover:bg-primary hover:bg-opacity-20 hover:text-white focus-visible:ring-primary focus-visible:ring-opacity-40 border-opacity-40';
 
     const sizeClasses = {
